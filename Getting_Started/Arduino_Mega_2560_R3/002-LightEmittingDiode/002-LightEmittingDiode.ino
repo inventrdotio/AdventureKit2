5933556e1dd208422a43b7e6b98e135936792775
David Schmidt <davids@tower-mt.com>
2023-01-20 22:58:55 -0800

    Fixed pin in code, added comment block

--- Getting_Started/Arduino_Mega_2560_R3/002-LightEmittingDiode/002-LightEmittingDiode.ino
+++ Getting_Started/Arduino_Mega_2560_R3/002-LightEmittingDiode/002-LightEmittingDiode.ino
@@ -1,4 +1,19 @@
-int Light = 12; // The HERO Board Pin that the LED is wired to
+/*
+ * Day 0 - AI Apocalypse by inventr.io
+ * Learn more at https://inventr.io/PLACEHOLDER
+ *
+ * Getting Started
+ * A light-emitting diode (LED) is a semiconductor device that emits light
+ * when an electric current is passed through it. LEDs are small, efficient,
+ * and long-lasting, making them ideal for a wide range of applications such
+ * as indicator lights, digital displays, and lighting. They are available
+ * in a variety of colors and sizes.
+ *
+ * Alex Eschenauer
+ * David Schmidt
+ */
+
+int Light = 13; // The HERO Board Pin that the LED is wired to
 
 // the setup function runs once when you press reset or power the board
 void setup() {
