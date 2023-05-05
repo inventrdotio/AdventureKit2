22734fc9691bb00a28d7f4c191885a61d306f615
David Schmidt <davids@tower-mt.com>
2023-05-04 20:15:20 -0700

    Keypad door lock sketches

--- Lessons/Chapter02_BaseSecurity/02_KeypadDoorLock/40_KeypadDoorLock/40_KeypadDoorLock.ino
+++ Lessons/Chapter02_BaseSecurity/02_KeypadDoorLock/40_KeypadDoorLock/40_KeypadDoorLock.ino
@@ -13,17 +13,17 @@
  * providing a total of 16 keys. Each key is labeled with a number, letter, or symbol,
  * such as 0-9, A-D, *, and #.
  *
- * The keypad has an 8-pin interface that simplifies connecting it to an Arduino board.
- * Internally, the keys are arranged in rows and columns, with one side of each button
- * connected to a row line and the other side connected to a column line. When a button
- * is pressed, it connects a specific row and column, creating a closed circuit.
+ * Let's make the feedback even better by adding an LCD1602 LCD character display.  That
+ * will allow us to give better instructions and feedback as the user enters the secret
+ * code.
  *
- * Here's a brief overview of the keypad's features:
+ * LCD stands for "Liquid Crystal Display." You might have seen these types of screens on
+ * calculators, microwaves, or digital clocks. The "1602" part means that this display has
+ * 16 columns and 2 rows, so you can show a total of 32 characters at once.
  *
- * 1) 4 rows and 4 columns, providing a total of 16 keys.
- * 2) Compact and flexible membrane design, making it easy to integrate into various projects.
- * 3) 8-pin interface, simplifying connection to an Arduino board.
- * 4) Can be easily programmed using the pre-loaded Keypad library.
+ * The LCD1602 is like a tiny TV screen that shows letters, numbers, and symbols. It has a
+ * special controller that helps us communicate with it using our Arduino board. We can
+ * send messages to the screen and tell it what to display.
  *
  * Alex Eschenauer
  * David Schmidt
@@ -36,12 +36,9 @@
  * - Pulse Width Modulation (PWM)
  */
 #include "Arduino.h"
-/*
- * The Keypad library is built using classes, which are a core concept in object-oriented
- * programming (OOP). Classes provide a way to bundle data and functions that operate on
- * that data within a single unit, making it easier to design, develop, and maintain code.
- */
-#include <Keypad.h>  // Keypad library is already included with the Arduino IDE
+
+#include <Keypad.h>         // Library for 4x4 membrane keypad
+#include <LiquidCrystal.h>  // LCD1602 library
 
 /* Choosing what pin to use.
  * This project only needs digital pins.  So, on the Hero XL we *could* use any digital or analog pin
@@ -61,100 +58,107 @@
  *    D22-D43, D47-D49, A8-A15
  */
 
-// Define number of ROWS and COLUMNS to use on our 4x4 keypad
 const byte ROWS = 4;
 const byte COLS = 4;
 
-// Define all pins used by our keypad.  For ease of wiring use pins in a single line
-// on the end of our HERO XL.
 const byte ROW_PINS[ROWS] = { 23, 25, 27, 29 };
 const byte COL_PINS[COLS] = { 31, 33, 35, 37 };
 
-// Define which characters will be returned to the sketch when each button
-// is pressed.  These values just match the labels for minimum confusion.
-const char BUTTONS[ROWS][COLS] = {
+char BUTTONS[ROWS][COLS] = {
   { '1', '2', '3', 'A' },  // 1st row
   { '4', '5', '6', 'B' },  // 2nd row
   { '7', '8', '9', 'C' },  // 3rd row
   { '*', '0', '#', 'D' }   // 4th row
 };
 
-/*
- * In the context of the Keypad library, the main class is the Keypad class. This class
- * encapsulates the functionality required to interface with a keypad, including reading
- * key presses and handling the keypad's rows and columns. When you create an instance of
- * the Keypad class, you need to provide the key mapping, row pins, column pins, and the
- * number of rows and columns as parameters.
- */
 Keypad security_keypad = Keypad(makeKeymap(BUTTONS), ROW_PINS, COL_PINS, ROWS, COLS);
 
-// Define our unlock combination here.  For testing we'll use a 4 digit code, but
-// the code will automatically use whatever length set here.  We will use the String
-// class as it provides a convenient way to handle, manipulate, and store sequences of
-// characters, such as the entered code in this sketch.  The String class is part of
-// the Arduino library.
-const String SECRET_CODE = "1234";
+LiquidCrystal lcd(22, 24, 26, 28, 30, 32);
 
+const String SECRET_CODE = "1234";
+const int code_indent = (16 - SECRET_CODE.length()) / 2;
 String entered_code = "";
+String prompt = "Enter Pass Code";
 
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16, 2);   // Set LCD resolution (16 columns, 2 rows)
+  lcd.clear();
+  showPrompt(prompt);
 }
 
 void loop() {
-  // Get a key if one has been pressed (returns 0/false when no key pressed)
   char key = security_keypad.getKey();
 
-  // If a keypad button has been pressed, handle it here
   if (key) {
     Serial.print("Key entered: ");
     Serial.println(key);
 
-    // Handle special BUTTONS:
-    // '*' Reset door to starting condition
-    // '#' Delete last button press (back up one)
+    // Reset key
     if (key == '*') {
       Serial.println("Reset: starting over");
+      lcd.clear();
+      showPrompt(prompt);
       entered_code = "";   // Reset back to starting point (nothing entered yet)
       return;             // exits loops here
     }
 
+    // Delete key
     if (key == '#') {
       Serial.println("Delete last character entered");
-      // String.substring(begin, end) returns a portion of a given string, starting
-      // with the index of the first character to return (starting at zero)
-      // and it returns all characters up to BUT NOT INCLUDING the second index.
-      // If we get all characters from the start (0) through one LESS than the length
-      // of the String, we have effectively removed the last character of the
-      // entered code.
+      lcd.setCursor(code_indent + entered_code.length() - 1, 1);
+      lcd.print(" ");
       entered_code = entered_code.substring(0, entered_code.length() - 1);
       Serial.print("  Current entered code: ");
       Serial.println(entered_code);
       return;
     }
 
-    entered_code += key;   // Add key to the end of our entered combination
+    entered_code += key;
 
-    // Log when someone begins to enter a combination
     if (entered_code.length() == 1) {
       Serial.println("A code has begun to be entered");
+      lcd.clear();
+      showPrompt("Complete code");
     }
+    lcd.setCursor(code_indent + entered_code.length() - 1, 1);
+    lcd.print("*");
 
     Serial.print("  Current entered code: ");
     Serial.println(entered_code);
 
-    // If we have collected enough characters compare to the unlock code
     if (entered_code.length() == SECRET_CODE.length()) {
       Serial.println("Entry complete - validating combination");
+      lcd.clear();
 
-      if (entered_code == SECRET_CODE) {   // Correct code entered!
+      if (entered_code == SECRET_CODE) {
         Serial.println("Entered code matches - unlock door for 2 seconds");
+        lcd.print("Door Unlocked");
         delay(2000);
         Serial.println("Re-lock door");
       } else {
+        lcd.print("No match");
+        lcd.setCursor(0, 1);
+        lcd.print("Door is locked");
         Serial.println("Wrong combination entered");
+        delay(2000);
+        Serial.println("Door locked");
       }
-      entered_code = "";   // clear code so user can try again
+      lcd.clear();
+      showPrompt(prompt);
+      entered_code = "";
     }
   }
+}
+
+void showPrompt(String prompt) {
+  int prompt_length = prompt.length();
+  Serial.println(prompt.c_str());
+  if (prompt_length > 16) {
+    Serial.println("Prompt is too long and will be truncated");
+    prompt.substring(0, 16);
+  }
+  int centering_offset = (16 - prompt_length) / 2;
+  lcd.setCursor(centering_offset, 0);
+  lcd.print(prompt.c_str());
 }
