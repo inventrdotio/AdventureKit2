002e9f0c30cd064940c63758d25358115af1460e
David Schmidt <davids@tower-mt.com>
2023-05-09 18:22:48 -0700

    chapter 2, lesson 3 final sketch (hopefully)

--- Lessons/Chapter02_BaseSecurity/03_RFID-DoorLocks/40_RFID-DoorLocks/40_RFID-DoorLocks.ino
+++ Lessons/Chapter02_BaseSecurity/03_RFID-DoorLocks/40_RFID-DoorLocks/40_RFID-DoorLocks.ino
@@ -17,31 +17,9 @@
  * David Schmidt
  */
 
-/*
-CODE FROM https://www.youtube.com/watch?v=Kyz1w_T-MGo
-Interconnections from the RFID to the Arduino pins:
-Reset     > Pin 5
-SS        > Pin 10
-MOSI      > Pin 11
-MISO      > Pin 12
-SCK       > Pin 13
-Ground    > Ground
-3.3v      > 3.3v
 
-
-Interconnections from the LCD (I2C) to the Arduino pins:
-GND > GND of the Arduino
-VCC > +5V of the Arduino
-SDA > A4 pin of the Arduino
-SCL > A5 pin of the Arduino
-*/
-
-
-#include <SPI.h>
 #include <MFRC522.h>
-// #include "pitches.h"
 #include <LiquidCrystal.h>
-// #include <Wire.h>
 
 const uint8_t RST_PIN = 26;  // Configurable, see typical pin layout above
 const uint8_t SS_PIN = 53;   // Configurable, see typical pin layout above
@@ -49,61 +27,111 @@ const uint8_t SS_PIN = 53;   // Configurable, see typical pin layout above
 MFRC522_SPI spiDevice = MFRC522_SPI(SS_PIN, RST_PIN);
 MFRC522 rfid = MFRC522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
+/*
+ * This is a two dimentional array that will contain the RFID codes that will
+ * open our doors.  RFID codes can contain 4, 7 or 10 bytes so we allocate a 10
+ * byte array for EACH code.  The empty braces [] indicate that the number of
+ * approved codes (and space for them) is set by how many codes are defined.
+ *
+ * If we define 2 codes this will allocate 2 arrays of 10 bytes, or 20 bytes.
+ * Each code consists of up to 10 bytes, each of which may be defined by a *decimal*
+ * number from 0-255.
+ *
+ * We start with NO defined codes.  We will then run the sketch and display the
+ * codes of key cards or fobs that we pass by the sensor.  We can then add approved
+ * codes that are allowed to open our doors.
+ */
 const byte APPROVED[][10] = {
-  { 217, 205, 16, 20 }  // Recognized key fob
+  // {},         // Add additional codes with commas (',') between each sequence
+  {34, 29, 168, 30}  // Recognized key fob
 };
 
+// Pins for our 16x2 LCD display
 LiquidCrystal lcd(22, 24, 23, 25, 27, 29);
 
 void setup() {
   Serial.begin(9600);
-  SPI.begin();
-  rfid.PCD_Init();
 
+  rfid.PCD_Init();  // Initialize RFID reader
+
+  // Initialize LCD display with our door prompt
   lcd.begin(16, 2);
   lcd.clear();
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0, 0);  // Go to beginning of first line
   lcd.print("  Tap to Enter  ");
 }
 
 void loop() {
-  // Look for new cards
+  // When a new RFID "card" is detected
   if (rfid.PICC_IsNewCardPresent()) {
 
-    // Select one of the cards
+    // Read the RFID code on the card.
     if (rfid.PICC_ReadCardSerial()) {
 
-      if (rfid.uid.size == 0) {
-        Serial.println("Bad card (size = 0)");
-      } else {
-        char tag[sizeof(rfid.uid.uidByte) * 4] = { 0 };
+      // If we were able to read a valid RFID (size > 0)
+      if (rfid.uid.size > 0) {
+        // Allocate enough space for us to display each byte in the code
+        // as a decimal number separated by ", " (like "255, 255, 255, 255")
+        char tag[sizeof(rfid.uid.uidByte) * 5] = { 0 };
         Serial.print("UID size: ");
         Serial.println(rfid.uid.size);
         for (int i = 0; i < rfid.uid.size; i++) {
-          char buff[5];  // 3 digits, dash and \0.
-          snprintf(buff, sizeof(buff), "%s%d", i ? "-" : "", rfid.uid.uidByte[i]);
+          char buff[6];  // 3 digits, ',', space and \0.
+          snprintf(buff, sizeof(buff), "%s%d", i ? ", " : "", rfid.uid.uidByte[i]);
           strncat(tag, buff, sizeof(tag));
         };
         Serial.println("Good scan: ");
         Serial.println(tag);
 
-        Serial.println("Comparing: ");
+        Serial.print("Comparing: ");
+        bool match_found = false;   // No matches found yet
+        // Check each of our approved codes.  Here we use a trick to determine how
+        // many codes we have.  We simply divide the total size of our APPROVED array
+        // and divide by the size of a single code.
         for (int i = 0; i < (sizeof(APPROVED) / sizeof(APPROVED[0])); i++) {
-          lcd.setCursor(0, 1);
-          if (strncmp(rfid.uid.uidByte, APPROVED[i], rfid.uid.size) == 0) {
-            Serial.println("Matched");
-            lcd.print("  Come on in!   ");
-          } else {
-            lcd.print("    No Match    ");
+          Serial.print("  Code: ");
+          Serial.println(i);
+          int j;
+          for (j = 0; j < rfid.uid.size; j++) {
+            Serial.print("  byte: ");
+            Serial.print(j);
+            char buff[8]; // space for 2 decimal bytes (0-255), a dash and terminating null
+            snprintf(buff, sizeof(buff), "  %d-%d", rfid.uid.uidByte[j], APPROVED[i][j]);
+            Serial.print(buff);
+            // Do byte comparison here.
+            if (rfid.uid.uidByte[j] != APPROVED[i][j]) {
+              Serial.println("  MISMATCH!");
+              break;    // break out of loop early
+            } else {
+              Serial.println();
+            }
           }
-          delay(2000);
-          lcd.setCursor(0, 1);
-          lcd.print("                ");
+          // If we find a mis-match we break out of our inner loop early, and
+          // j will be less than the size of the code we read.
+          if (j == rfid.uid.size) {
+            match_found = true;
+            break;    // exit outer loop since we found a match
+          }
         }
-      };
+        
+        // Now we display on the LCD whether a match was found or not
+        lcd.setCursor(0, 1);
+        if (match_found) {
+          Serial.println("Matched");
+          lcd.print("  Come on in!   ");
+          // UNLOCK THE DOOR HERE!
+        } else {
+          Serial.println("NO Match");
+          lcd.print("    NO MATCH    ");
+        }
+        delay(2000);
+        lcd.setCursor(0, 1);
+        lcd.print("                ");
+      } else {
+        Serial.println("Bad card (size = 0)");
+      }
 
       // disengage with the card.
-      //
       rfid.PICC_HaltA();
 
     } else {
