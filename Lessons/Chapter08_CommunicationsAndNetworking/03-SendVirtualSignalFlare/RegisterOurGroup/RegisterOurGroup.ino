22be6e6a4096a5e44111eac64baa4db8ee84b1b7
David Schmidt <davids@tower-mt.com>
2023-07-06 22:48:57 -0700

    Added first cut at Signal Flare registration

---
+++ Lessons/Chapter08_CommunicationsAndNetworking/03-SendVirtualSignalFlare/RegisterOurGroup/RegisterOurGroup.ino
@@ -0,0 +1,77 @@
+/*
+ * Day 0 - AI Apocalypse by inventr.io
+ * Learn more at https://inventr.io/PLACEHOLDER
+ *
+ * Let's join the other resistance groups that are battling the Global Phantom
+ * Technologies rogue AI agents!  The Day-0 group we have been communicating with
+ * has provided us with an API (Application Programming Interface) key that when
+ * combined with our email address will validate our identity.
+ *
+ * Alex Eschenauer
+ * David Schmidt
+ */
+
+// The following two libraries are included with the ESP32 board definitions.
+// If you get compile errors make sure you have the board set to "ESP32 Dev Module"
+// in the Arduino IDE settings.
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+// 
+#include "secrets.h"
+
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+
+  // Check if the secrets have been set
+  if (String(SSID) == "YOUR_SSID" || String(SSID_PASSWORD) == "YOUR_WIFI_PASSWORD"
+      || String(USER_EMAIL) == "YOUR_EMAIL_ADDRESS" || String(API_KEY) == "YOUR_API_KEY") {
+    Serial.println("Please update the secrets.h file with your credentials before running the sketch.");
+    Serial.println("You need to replace YOUR_SSID and YOUR_WIFI_PASSWORD with your WiFi credentials.");
+    Serial.println("Also replace YOUR_EMAIL_ADDRESS and YOUR_API_KEY with your API credentials.");
+    return;  // Stop further execution of the code
+  }
+
+  // Connect to Wi-Fi
+  WiFi.begin(SSID, SSID_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Initialize the HTTPClient object
+  HTTPClient http;
+
+  // Construct the URL
+  String url = "http://day-0.adventurekit2.com/survivors/register?";
+  url += "api_key=" + String(API_KEY);
+  url += "&email=" + String(USER_EMAIL);
+
+  // Make the HTTP GET request
+  http.begin(url);
+  int httpCode = http.GET();
+
+  // Check the return code
+  if (httpCode == HTTP_CODE_OK) {
+    // If the server responds with 200, print the response
+    String payload = http.getString();
+    Serial.println("Response:");
+    Serial.println(payload);
+  } else if (httpCode == HTTP_CODE_FORBIDDEN) {
+    // If the server responds with 403, print an error message
+    Serial.println("Invalid email or API key.");
+  } else {
+    // For any other HTTP response code, print it
+    Serial.println("Received unexpected HTTP response:");
+    Serial.println(httpCode);
+  }
+
+  // End the HTTP connection
+  http.end();
+}
+
+void loop() {
+  // do nothing
+}
