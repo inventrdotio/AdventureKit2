e76cb92aea2a76cd06d3909e20348708de7112e2
David Schmidt <davids@tower-mt.com>
2023-07-06 22:56:45 -0700

    force added secrets.h

---
+++ Lessons/Chapter08_CommunicationsAndNetworking/03-SendVirtualSignalFlare/RegisterOurGroup/secrets.h
@@ -0,0 +1,8 @@
+
+// Replace with your network details
+const char* SSID = "YOUR_SSID";
+const char* SSID_PASSWORD = "YOUR_WIFI_PASSWORD";
+
+// Replace with your API credentials
+const char* USER_EMAIL = "YOUR_EMAIL_ADDRESS";
+const char* API_KEY = "YOUR_API_KEY";
