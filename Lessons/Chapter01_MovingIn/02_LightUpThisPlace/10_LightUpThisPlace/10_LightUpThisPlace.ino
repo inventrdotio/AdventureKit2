e1dee75673e3b2d47bc670a1cdeaa081673d92a2
David Schmidt <davids@tower-mt.com>
2024-01-23 11:00:24 -0800

    fixed mispelling in C1L2 sketch

--- Lessons/Chapter01_MovingIn/02_LightUpThisPlace/10_LightUpThisPlace/10_LightUpThisPlace.ino
+++ Lessons/Chapter01_MovingIn/02_LightUpThisPlace/10_LightUpThisPlace/10_LightUpThisPlace.ino
@@ -88,7 +88,7 @@
  *       D0/D1 (used by USB and Serial)
  *       D14-D19 (used by Serial 1-3)
  *       D2/D3, D18-D21 (used for external interrupts)
- *       D13 (this pin controls the build in LED on the HERO XL board (LED_BUILTIN)),
+ *       D13 (this pin controls the built in LED on the HERO XL board (LED_BUILTIN)),
  *       D2-D13, D44-D46 (used for Pulse Width Modulation (PWM))
  *       D50 (MISO), D51 (MOSI), D52 (SCK), D53 (SS).  (used for SPI communication)
  *       D20 (SDA), D21 (SCL).  (used for I2C communication using the Wire library)
